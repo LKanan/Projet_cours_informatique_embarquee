e003a6ac0b31188df37802e5bc5ae8f16a17ecf4
LKanan <lucienkanani@gmail.com>
2025-02-17 08:30:48 +0200

    Code de la premiere carte finie

---
+++ CARTE1/CARTE1.ino
@@ -0,0 +1,114 @@
+// Bibliothèque pour les cartes LCD
+#include <LiquidCrystal.h>
+
+// Bibliothèque pour le servo moteur
+#include <Servo.h>
+
+// Définition des broches pour les LCD
+// Broches partagées
+#define D4 2
+#define D5 3
+#define D6 4
+#define D7 5
+
+// Définition des broches spécifiques pour chaque écran
+#define E1 6   // Enable pour l'écran 1
+#define RS1 7  // Register Select pour l'écran 1
+#define E2 8   // Enable pour l'écran 2
+#define RS2 9  // Register Select pour l'écran 2
+
+// Initialisation des écrans LCD avec broches partagées
+LiquidCrystal lcd1(RS1, E1, D4, D5, D6, D7);
+LiquidCrystal lcd2(RS2, E2, D4, D5, D6, D7);
+
+// Declaration des pins des leds des feux
+int ledsVR = 10;
+int ledsJ = 11;
+
+// Declaration de l'objet Servo
+int servoPin = 12;
+Servo moteur;
+
+// unsigned long dureeMaxTemporisatonFeuRV = 0;
+// unsigned long dureeMaxTemporisatonFeuJ = 0;
+unsigned long tempsMillis = 0;
+
+
+void setup() {
+  // Initialisations pour la commmunication série des deux cartes
+  Serial.begin(9600);
+
+  // Initialisation pour les LCD
+  lcd1.begin(16, 2);  // Configuration de l'écran 1 (16 colonnes, 2 lignes)
+  lcd2.begin(16, 2);
+  // lcd2.blink()
+
+  // Initialisation du servo moteur
+  // moteur.attach(servoPin, 650, 2662);
+
+  moteur.attach(servoPin, 550, 2662);
+
+  // Initialisation des pins des leds du feu
+  pinMode(ledsVR, OUTPUT);
+  pinMode(ledsJ, OUTPUT);
+}
+
+void loop() {
+  afficherMessage(lcd1,"hello",10);
+  afficherMessage(lcd2,"coucou",10);
+  allumerFeu(1000,500);
+  // Test LCD
+  // for (int i = duree; i >= 0; i--) {
+  //   afficherMessage(lcd1, "STOP", i);
+  //   afficherMessage(lcd2, "PASS", i);
+  //   delay(1000);
+  // }
+
+  // for (int i = duree; i >= 0; i--) {
+  //   afficherMessage(lcd1, "PASS", i);
+  //   afficherMessage(lcd2, "STOP", i);
+  //   delay(1000);
+}
+
+
+// Fonction d'affichage du message avec compte à rebours
+void afficherMessage(LiquidCrystal &lcd, String message, int duree) {
+  lcd.clear();          // Efface l'écran avant l'affichage
+  lcd.setCursor(0, 0);  // Positionne le curseur en haut à gauche
+  lcd.print(message);   // Affiche le message sur la première ligne
+  lcd.setCursor(0, 1);  // Positionne le curseur sur la deuxième ligne
+  lcd.print("Temps : ");
+  lcd.print(duree);
+  lcd.print("s    ");  // Espaces pour éviter les chiffres résiduels
+}
+
+void tournerServo(int angle, Servo servomoteur) {
+  // 79 pour 90 deg
+  // 170 pour 11730 deg
+  servomoteur.write(angle);
+}
+
+void allumerFeu(int dureeMaxTemporisatonFeuRV, int dureeMaxTemporisatonFeuJ) {
+  if ((millis() - tempsMillis) < dureeMaxTemporisatonFeuRV) {
+    digitalWrite(ledsVR, 0);
+    digitalWrite(ledsJ, 1);
+  tournerServo(90,moteur);
+  } else if (((SoustraireMillis() > dureeMaxTemporisatonFeuRV) && (SoustraireMillis() < dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ)) || ((SoustraireMillis() > dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ * 2) && (SoustraireMillis() < dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ * 3)) || ((SoustraireMillis() > dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ * 4) && (SoustraireMillis() < dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ * 5))) {
+    digitalWrite(ledsVR, 1);
+    digitalWrite(ledsJ, 0);
+  } else if (((SoustraireMillis() > dureeMaxTemporisatonFeuRV + dureeMaxTemporisatonFeuJ * 5) && (SoustraireMillis() < (dureeMaxTemporisatonFeuRV * 2) + dureeMaxTemporisatonFeuJ * 6))) {
+    digitalWrite(ledsVR, 0);
+    digitalWrite(ledsJ, 1);
+  tournerServo(0,moteur);
+  } else {
+    digitalWrite(ledsVR, 1);
+    digitalWrite(ledsJ, 1);
+  }
+  if ((SoustraireMillis() > (dureeMaxTemporisatonFeuRV * 2) + dureeMaxTemporisatonFeuJ * 6)) {
+    tempsMillis = millis();
+  }
+}
+long SoustraireMillis() {
+  return millis() - tempsMillis;
+  tournerServo(90,moteur);
+}
---
+++ CARTE2/CARTE2.ino
@@ -0,0 +1,52 @@
+// Declaration des capteurs ultrasoniques
+const int trigsPin = 2;
+const int echoPin1 = 3;
+const int echoPin2 = 4;
+const int echoPin3 = 5;
+const int echoPin4 = 6;
+const int echoPin5 = 7;
+const int echoPin6 = 8;
+
+
+
+void setup() {
+  // Initialisations pour la commmunication série des deux cartes
+  Serial.begin(9600);
+
+  // Initialisations pour les capteurs ultasoniques
+  pinMode(trigsPin, OUTPUT);
+  for (int i=echoPin1; i<=echoPin6; i++) {
+  pinMode(i, INPUT);
+  }
+  digitalWrite(trigsPin, LOW);
+}
+
+void loop() {
+  // Serial.print("Capteur1 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin1));
+  // Serial.print("Capteur2 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin2));
+  // Serial.print("Capteur3 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin3));
+  // Serial.print("Capteur4 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin4));
+  // Serial.print("Capteur5 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin5));
+  // Serial.print("Capteur6 : ");
+  // Serial.println(mesureCapeurUltrason(echoPin6));
+  // Serial.println("============================================================");
+  // Serial.println("");
+}
+
+long mesureCapeurUltrason(int echoPin){
+  digitalWrite(trigsPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigsPin, LOW);
+  delayMicroseconds(2);
+  long dureeRenvoyee = pulseIn(echoPin, HIGH);
+  long distanceRenvoyee = (dureeRenvoyee * 0.0344) / 2; // Calcul de la distance en cm
+  delay(150);
+
+  return distanceRenvoyee;
+}
+
